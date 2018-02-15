4c9c1f99f4e7df1b4fcf2aeaf38b2f3b25968f36
nju33 <nju33.ki@gmail.com>
2018-02-16 01:12:13 +0900

    docs: Update demo

--- docs/index.js
+++ docs/index.js
@@ -1,26 +1,53 @@
-(() => {
-  // eslint-disable-next-line no-undef, no-unused-vars
-  const calendar = new ApocCalendar({
-    target: document.getElementById('calendar'),
-    data: {
-      head(year, month) {
-        return `${year}年${month}月`;
-      },
-      // day(day) {
-      //   return 'a';
-      // },
+/* eslint-disable no-undef */
 
-      // min: '2018-01-01',
-      max: '2018-12-31',
-    },
+(() => {
+  const now = new Date();
+  // eslint-disable-next-line
+  const multiLeft = new ApocCalendar(
+    document.getElementById('multi-left'),
+    {
+      'date.min': now,
+      'date.max': dateFns.addMonths(now, 13),
+      'pager.next': false,
+    }
+  );
+
+  // eslint-disable-next-line
+  const multiRight = new ApocCalendar(
+    document.getElementById('multi-right'),
+    {
+      ref: multiLeft,
+      'pager.prev': false,
+      'pager.next': false,
+    }
+  );
+
+  // eslint-disable-next-line
+  const multiRight2 = new ApocCalendar(
+    document.getElementById('multi-right2'),
+    {
+      ref: multiLeft,
+      'pager.prev': false,
+    }
+  );
+
+  multiLeft.on('onUpdateDates', dates => {
+    console.log(dates);
   });
-  calendar.on('change', ({data}) => {
-    console.log(data);
-    window.data = data;
+  multiLeft.on('onReachLowerLimit', function () {
+    console.log('aaaaa');
+    console.log(arguments);
+  });
+  multiLeft.on('onReachUpperLimit', function () {
+    console.log(arguments);
   });
 
   document.getElementById('reset').addEventListener('click', () => {
-    console.log(calendar);
-    calendar.reset();
+    multiLeft.reset();
   });
+
+  multiLeft.restore([
+    '2018-02-15',
+    '2018-04-04',
+  ]);
 })();
